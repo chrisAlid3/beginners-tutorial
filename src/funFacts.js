861b80ab1827a0f1bcaa065f04a2d06e7bbcb0f3
chrisAlid3 <chrisbattberg@yahoo.com>
2023-04-17 08:47:14 +0200

    Initialize

---
+++ src/funFacts.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function FunFacts() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="./logo192.png" width="40px" alt="React-Logo"></img>
+        </nav>
+      </header>
+
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+
+      <footer>
+        <small>&copy; 2023 Bracher development. All right reserved</small>
+      </footer>
+    </div>
+  );
+}
+
+export default FunFacts;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Funfacts from "./funFacts";
+
+ReactDOM.render(<Funfacts />, document.getElementById("root"));
