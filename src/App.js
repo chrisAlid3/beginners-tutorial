46c4595595b88660b7381252ad3e15d3bbbd76ac
chrisAlid3 <chrisbattberg@yahoo.com>
2023-04-18 08:22:56 +0200

    Added import app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import "./style.css";
+import "./App.css";
 import Header from "./components/Header";
 import Funfacts from "./components/FunFacts";
 import Footer from "./components/Footer";
